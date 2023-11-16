25813c50e46e0966d55621f33702b32dc3fbef90
= <gablandry31@gmail.com>
2023-11-15 21:49:41 -0500

    Modification

--- src/components/HeaderContainer.vue
+++ src/components/HeaderContainer.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import IconFlagFR from './icons/IconFlagFR.vue'
 import IconFlagUSA from './icons/IconFlagUSA.vue'
 
-const language = navigator.language.startsWith('fr') ? 'fr' : 'en'
+const language = ref(navigator.language.startsWith('fr') ? 'fr' : 'en')
+
+const changeLanguage = () => {
+  language.value = language.value === 'fr' ? 'en' : 'fr'
+}
 </script>
 
 <template>
   <div class="header">
-    <div id="language-button">
+    <div id="language-button" @click="changeLanguage()">
       <IconFlagUSA v-if="language === 'fr'" />
       <IconFlagFR v-else />
     </div>
